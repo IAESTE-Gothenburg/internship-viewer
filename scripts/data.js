298fd7a447154491c900b27b3d52eff8683bcea0
Anton Levholm <anton.badminton@gmail.com>
2017-02-09 12:47:05 +0100

    update lists

--- scripts/data.js
+++ scripts/data.js
@@ -1,18 +1,18 @@
 const SECTIONS = ['A', 'AE', 'D', 'E', 'Fbw', 'H', 'I', 'IT', 'K', 'KfKb', 'M', 'S', 'TD', 'V', 'Z'];
 
 const A = ['BO-2017-DEL-01.jpg', 'CA-2017-000024.jpg', 'CN-2017-CDG-C01.jpg', 'NP-2017-000-CI.jpg', 'NP-2017-000085-CI.jpg', 'US-2017-89025.jpg', 'DE-2017-3021-3.jpg', 'GH-2017-0142.jpg'];
-const AE = ['BO-2017-DEL-01.jpg', 'CA-2017-000024.jpg', 'CN-2017-CDG-C01.jpg', 'NP-2017-000-CI.jpg', 'NP-2017-000085-CI.jpg', 'US-2017-89025.jpg', 'GH-2017-0142.jpg'];
+const AE = ['CN-2017-CDG-C01.jpg', 'NP-2017-000-CI.jpg', 'NP-2017-000085-CI.jpg', 'US-2017-89025.jpg', 'GH-2017-0142.jpg'];
 const D = ['BE-2017-088UGE.jpg', 'CH-2017-000102.jpg', 'NP-2017-000085-CI.jpg', 'UA-2017-0001.jpg'];
 const E = ['CN-2017-XYM-C06.jpg', 'NP-2017-000-CI.jpg', 'NP-2017-000054-CI.jpg'];
 const Fbw = ['CH-2017-000011.jpg', 'CH-2017-000102.jpg', 'IN-2017-2701-MU.jpg'];
-const H = ['DE-2017-3021-3.jpg', 'GH-2017-0142.jpg'];
+const H = ['DE-2017-3021-3.jpg', 'GH-2017-0142.jpg', 'NP-2017-000085-CI.jpg'];
 const I = ['CN-2017-MYP-B02.jpg', 'IR-2017-1202.jpg', 'PS-2017-151-IND.jpg', 'UA-2017-0001.jpg', 'CN-2017-WAN-B19.jpg', 'CN-2017-WAN-C16.jpg', 'GH-2017-0142.jpg', 'IN-2017-3423-KU.jpg'];
 const IT = ['BE-2017-088UGE.jpg', 'CH-2017-000102.jpg', 'NP-2017-000085-CI.jpg', 'UA-2017-0001.jpg', 'IN-2017-3423-KU.jpg'];
 const K = ['CN-2017-WAN-B19.jpg', 'CN-2017-WAN-C16.jpg', 'RS-2017-1178.jpg', 'GH-2017-0142.jpg', 'US-2017-89051.jpg'];
 const KfKb = ['RS-2017-1178.jpg', 'US-2017-89051.jpg', 'GH-2017-0142.jpg'];
-const M = ['AT-2017-5003LZ.jpg', 'BR-2017-0205ES.jpg', 'CH-2017-000005.jpg', 'CH-2017-000102.jpg', 'CN-2017-MET-C15.jpg', 'CN-2017-MET-C20.jpg','CN-2017-MYP-B02.jpg', 'CZ-2017-010005.jpg', 'KZ-2017-0024.jpg', 'UA-2017-0001.jpg', 'GH-2017-0142.jpg'];
+const M = ['AT-2017-5003LZ.jpg', 'BR-2017-0205ES.jpg', 'CH-2017-000005.jpg', 'CH-2017-000102.jpg', 'CN-2017-MET-C15.jpg', 'CN-2017-MET-C20.jpg','CN-2017-MYP-B02.jpg', 'CZ-2017-010005.jpg', 'KZ-2017-0024.jpg', 'UA-2017-0001.jpg', 'GH-2017-0142.jpg', 'CN-2017-XYM-C06.jpg'];
 const S = ['GH-2017-0142.jpg'];
-const TD = ['CZ-2017-010005.jpg', 'DE-2017-3021-3.jpg', 'GH-2017-0142.jpg', 'IN-2017-3423-KU.jpg'];
+const TD = ['CZ-2017-010005.jpg', 'DE-2017-3021-3.jpg', 'GH-2017-0142.jpg', 'IN-2017-3423-KU.jpg', 'UA-2017-0001.jpg', 'PS-2017-151-IND.jpg'];
 const V = ['ES-2017-003306.jpg', 'ES-2017-007001.jpg', 'HU-2017-0014.jpg', 'NP-2017-000-CI.jpg', 'NP-2017-000085-CI.jpg', 'UK-2017-308-01.jpg', 'UK-2017-308-02.jpg', 'UK-2017-308-03.jpg', 'UK-2017-308-04.jpg', 'UK-2017-308-08.jpg', 'GH-2017-0142.jpg',];
 const Z = ['KZ-2017-0024.jpg', 'CH-2017-000102.jpg', 'GH-2017-0142.jpg'];
 const OTHER = ['VN-2017-ITC-10.jpg'];
